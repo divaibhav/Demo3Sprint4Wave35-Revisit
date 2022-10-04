4ab2641e4762dda8c1718a53a7d1844ebfd2b7ba
Vaibhav Diwan <vaibhav.diwan@niit.com>
2022-10-04 20:22:15 +0530

    updated the calculateIncrement method argument

--- src/com/example/data/Employee.java
+++ src/com/example/data/Employee.java
@@ -6,4 +6,55 @@ public class Employee {
     private String dob;
     private String address;
     private double salary;
+
+    public Employee(String name, int age, String dob, String address, double salary) {
+        this.name = name;
+        this.age = age;
+        this.dob = dob;
+        this.address = address;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    public double calculateIncrement(){
+        return (salary * 5.0)/100;
+    }
 }
